ad64ede2147c531e74cf5f4cc8f4edfeb2504726
Prasad Kute <prasadkute0708@gmail.com>
2024-12-15 10:55:06 +0530

    added db connection

--- server/index.js
+++ server/index.js
@@ -1,5 +1,16 @@
-import express from "express";
+import express from 'express';
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-app.listen();
+// Middleware
+app.use(express.json());
+
+// Start the server
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

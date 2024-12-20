1ec77c694ae5c6ccec45a823ae2175e8b5dac902
Prasad Kute <prasadkute0708@gmail.com>
2024-12-20 18:37:02 +0530

    logout corrected

--- client/src/portals/recruiter/pages/Dashboard.jsx
+++ client/src/portals/recruiter/pages/Dashboard.jsx
@@ -1,13 +1,14 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { AuthContext } from '../../../context/AuthContext';
+
 import Button from '../../../components/common/Button';
 import api from '../../../services/api';
 import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/solid';
+import { useAuth } from '../../../contexts/AuthContext';
 
 const RecruiterDashboard = () => {
   const navigate = useNavigate();
-  const { logout } = useContext(AuthContext);
+  const { logout } = useAuth();
   const [jobPostings, setJobPostings] = useState([]);
   const [expandedJobs, setExpandedJobs] = useState({});
   const [loading, setLoading] = useState(true);

4e41920479c55372c61715365a773b167e552030
Prasad Kute <prasadkute0708@gmail.com>
2024-12-27 18:48:54 +0530

    normal changes

--- client/src/RefreshHandler.jsx
+++ client/src/RefreshHandler.jsx
@@ -1,5 +1,5 @@
-import React, {useEffect} from 'react'
-import { use } from 'react';
+import  {useEffect} from 'react'
+
 import { useLocation, useNavigate } from 'react-router-dom'
 
 export default function RefreshHandler({ setIsAuthenticated }) {

e0fca88587ec83ab965cf68eed357182815d334b
Pranavaaaa <pranav.22210388@viit.ac.in>
2024-12-27 09:31:51 +0530

    Auth Part done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import CandidateRoutes from './routes/CandidateRoutes';
@@ -7,21 +7,31 @@ import Login from './components/auth/LoginForm';
 import NotFound from './portals/shared/NotFound';
 import RegistrationForm from './components/auth/RegistrationForm';
 import { Toaster } from 'react-hot-toast';
+import { Navigate } from 'react-router-dom';
+import RefreshHandler from './RefreshHandler';
 
 function App() {
+
+  const [isAuthenticated, setIsAuthenticated] = React.useState(false);
+
+  const PrivateRoute = ({ element }) => {
+    return isAuthenticated ? element : <Navigate to="/login" />;
+  };
+
   return (
     <AuthProvider>
       <Router>
-      <Toaster />
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<RegistrationForm />} />
-          <Route path="/candidate/*" element={<CandidateRoutes />} />
-          <Route path="/recruiter/*" element={<RecruiterRoutes />} />
-          <Route path="/unauthorized" element={<div>unauthorized</div>} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+      <RefreshHandler setIsAuthenticated = {setIsAuthenticated}/>
+        <Toaster />
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<RegistrationForm />} />
+            <Route path="/candidate/*" element={<CandidateRoutes />} />
+            <Route path="/recruiter/*" element={<PrivateRoute element = {<RecruiterRoutes/>} />} />
+            <Route path="/unauthorized" element={<div>unauthorized</div>} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
       </Router>
     </AuthProvider>
   );
---
+++ client/src/RefreshHandler.jsx
@@ -0,0 +1,23 @@
+import React, {useEffect} from 'react'
+import { use } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom'
+
+export default function RefreshHandler({ setIsAuthenticated }) {
+  
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+
+    if(localStorage.getItem('token')) {
+      setIsAuthenticated(true);
+      if(location.pathname === '/login' || location.pathname === '/register' || location.pathname === '/') {
+        navigate('/recruiter/dashboard', {replace: false});
+      }
+    }
+  }, [location, navigate, setIsAuthenticated]);
+
+  return (
+    null
+  )
+}
